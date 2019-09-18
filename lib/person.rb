a2af41d3a20b14440330ba6f599d8ed14ce5d0f3
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-18 19:27:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
 # your code goes here
+class Person
+  attr_accessor :bank_account
+  attr_reader :name
+  attr_writer
+  
+  def initialize(name,bank_account=25)
+    @name = name
+    @bank_account = bank_account
+  end
+end
